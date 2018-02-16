5f825f8baccf822c3afc05d12b6505b244e83b33
Sadik Ekin Ozbay <ozbaysa@itu.edu.tr>
2018-02-16 23:05:40 +0300

    Google maps added

---
+++ Gotur/Controllers/EnterenceVC.swift
@@ -0,0 +1,13 @@
+//
+//  EnterenceVC.swift
+//  Gotur
+//
+//  Created by Sadik Ekin Ozbay on 16.02.2018.
+//  Copyright © 2018 DDosJS-Attack. All rights reserved.
+//
+
+import UIKit
+
+class EnterenceVC: BaseVC {
+
+}
---
+++ Gotur/Controllers/MessengerVC.swift
@@ -0,0 +1,22 @@
+//
+//  MessengerVC.swift
+//  Gotur
+//
+//  Created by Sadik Ekin Ozbay on 16.02.2018.
+//  Copyright © 2018 DDosJS-Attack. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class MessengerVC: BaseVC {
+    
+    override func setupViews() {
+    }
+    override func setupAnchors() {
+        
+    }
+    override func fetchData() {
+        
+    }
+}
