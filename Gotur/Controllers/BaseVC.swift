a0f4cd39b8a8f90807a146c3e328fc35d9c03e50
ismail <i.namdar@yahoo.com>
2018-02-16 22:32:55 +0300

    Base Project added

---
+++ Gotur/Controllers/BaseVC.swift
@@ -0,0 +1,46 @@
+//
+//  BaseVC.swift
+//  Gotur
+//
+//  Created by Sadik Ekin Ozbay on 16.02.2018.
+//  Copyright © 2018 Sadik Ekin Ozbay. All rights reserved.
+//
+
+import UIKit
+
+class BaseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
+        fetchData()
+        setupViews()
+        setupAnchors()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
+    // It is responsible for creating the views
+    func setupViews(){
+        
+    }
+    
+    // It is responsible for setting up the anchors
+    func setupAnchors(){
+
+    }
+    
+    //API works will be in this function
+    func fetchData(){
+ 
+    }
+
+}
+
