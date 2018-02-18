f5e66ee8c8a9385d9ea2d02f410b35bd7bacccbb
Sadik Ekin Ozbay <ozbaysa@itu.edu.tr>
2018-02-18 09:42:29 +0300

    podfile and functional changes

--- Pods/Target Support Files/Socket.IO-Client-Swift/Socket.IO-Client-Swift-umbrella.h
+++ Pods/Target Support Files/Socket.IO-Client-Swift/Socket.IO-Client-Swift-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double SwiftSocketVersionNumber;
-FOUNDATION_EXPORT const unsigned char SwiftSocketVersionString[];
+FOUNDATION_EXPORT double SocketIOVersionNumber;
+FOUNDATION_EXPORT const unsigned char SocketIOVersionString[];
 
